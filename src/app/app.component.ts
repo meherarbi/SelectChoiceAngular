d60653d3107dbdf69b2cb7cc22f9ba0eaa6e327d
Arbi <developer.mediseo@gmail.com>
2023-01-14 15:35:11 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'select';
+  selectedSituation = 'Biparental';
+  selectedChoice :string='choix 1';
+  choices = ['choix 1', 'choix 2', 'choix 3'];
+  enabledChoices :{[key:string]:{[key:string]:boolean}} = {
+    'Biparental': {'choix 1': true, 'choix 2': true, 'choix 3': false},
+    'Monoparental': {'choix 1': false, 'choix 2': false, 'choix 3': true},
+  };
+  choiceDisabled = false;
+  updateChoices() {
+    this.choices = Object.entries(this.enabledChoices[this.selectedSituation])
+    .filter(([choice,enabled]) => enabled)
+    .map(([choice,enabled]) => choice);
+    this.choiceDisabled = !Object.values(this.enabledChoices[this.selectedSituation]).some(value => value);
+  }
 }
+
+
+
+
+
+
